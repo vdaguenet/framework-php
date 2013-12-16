43294d3c6529389e167c8410e2b2a8fa03daca8f
vdaguenet <valentin.daguenet@gmail.com>
2013-12-16 15:47:51 +0100

    Complete profile update view

--- web/views/User/update.php
+++ web/views/User/update.php
@@ -10,15 +10,33 @@
 			</div>
 		<?php endif; ?>
 
-		<div class="well">
-			<p>Votre e-mail actuel : <?php echo $user->getEmail(); ?></p>
-		</div>
-
 		<form method="POST" class="form-horizontal" enctype="multipart/form-data">
+			<div class="form-group">
+				<label class="col-sm-2 control-label">Login</label>
+				<div class="col-sm-10">
+					<p class="form-control-static"><?php echo $user->getUsername(); ?></p>
+				</div>
+			</div>
+			
+			<div class="form-group">
+				<label for="email" class="col-sm-2 control-label">New password</label>
+				<div class="col-sm-10">
+					<input type="password" class="form-control" name="password" id="password" placeholder="new password">
+				</div>
+			</div>
+
 			<div class="form-group">
 				<label for="email" class="col-sm-2 control-label">E-mail</label>
 				<div class="col-sm-10">
-					<input type="email" class="form-control" name="email" id="email" placeholder="example@mail.com">
+					<input type="email" class="form-control" name="email" id="email" placeholder="<?php echo $user->getEmail(); ?>">
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="avatar" class="col-sm-2 control-label">Avatar</label>
+				<div class="col-sm-10">
+					<p class="form-control-static"><img src="<?php echo './web/assets/img/avatars/' . $user->getAvatar(); ?>" class="img-circle" width="140px"></p>
+					<input type="file" class="form-control" name="avatar" id="avatar">
 				</div>
 			</div>
 
